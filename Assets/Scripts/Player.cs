76dd485a46bff1f60c4f1a099859a569272914e7
Yj621 <87031282+Yj621@users.noreply.github.com>
2024-11-20 17:52:25 +0900

    [FEAT] Player Class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,127 +1,40 @@
-using System.Collections;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player
 {
-    public float speed = 5;
-    public float jumpSpeed = 5;
-    public Collider2D bottomCollider;
-    public CompositeCollider2D terrainCollider;
+    public int Hp { get; private set; }
+    public int Damage { get; private set; }
+    public int Experience { get; private set; }
 
-    Rigidbody2D rb;
-
-    float prevVx = 0;
-    float vx = 0;
-    bool isGround;
-    bool goIdle;
-
-    void Start()
+    
+    public Player(int hp, int damage, int experience)
     {
-        rb = GetComponent<Rigidbody2D>();   
+        Hp = hp;
+        Damage = damage;
+        Experience = experience;
     }
 
-    void Update()
+    public void Attack(int damage) 
     {
-        vx = Input.GetAxisRaw("Horizontal") * speed;
-        float vy = GetComponent<Rigidbody2D>().linearVelocityY;
-
-        if (vx < 0)
+        Hp -= damage;
+        if(Hp < 0)
         {
-            GetComponent<SpriteRenderer>().flipX = true;
-        }
-        if (vx > 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
-
-        //땅에 닿아있는가
-        if (bottomCollider.IsTouching(terrainCollider))
-        {
-            if (!isGround)
-            {
-                if (vx == 0)
-                {
-                    GetComponent<Animator>().SetTrigger("Idle");
-                }
-                else
-                {
-                    GetComponent<Animator>().SetTrigger("Walk");
-                }
-            }
-            else
-            {
-                if (vx != prevVx)
-                {
-                    if (vx == 0)
-                    {
-                        GetComponent<Animator>().SetTrigger("Idle");
-                    }
-                    else
-                    {
-                        GetComponent<Animator>().SetTrigger("Walk");
-                    }
-                }
-                else if(goIdle)
-                {
-                    GetComponent<Animator>().SetTrigger("Idle");
-                    goIdle = false;
-                }
-
-            }
-        }
-        else
-        {
-            if (isGround)
-            {
-                GetComponent<Animator>().SetTrigger("Jump");
-            }
-        }
-
-        //플레이어 발이 바닥 콜라이더와 닿았다면
-        isGround = bottomCollider.IsTouching(terrainCollider);
-
-        if (Input.GetButtonDown("Jump") && isGround == true)
-        {
-            vy = jumpSpeed;
-        }
-        prevVx = vx;
-
-        GetComponent<Rigidbody2D>().linearVelocity = new Vector2(vx, vy);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            GetComponent<Animator>().SetTrigger("Attack1");
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            GetComponent<Animator>().SetTrigger("Attack2");
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            GetComponent<Animator>().SetTrigger("BowAttack");
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            GetComponent<Animator>().SetTrigger("RightAttack");
+            Hp = 0;
         }
     }
 
-
-    //애니메이션 스프라이트 문제 해결 함수
-    void UpY()
-    {
-        transform.position = new Vector3(transform.position.x, transform.position.y + 0.4f, transform.position.z);
-        rb.gravityScale = 0;
-    }
-    void DownY()
-    {
-        transform.position = new Vector3(transform.position.x, transform.position.y - 0.4f, transform.position.z);
-        rb.gravityScale = 4;
-        isGround = false;
+    public void Heal(int damage) 
+    { 
+        Hp += damage;   
     }
 
-    void SetGoIdle()
+    public void GetExperience(int exp)
     {
-        goIdle = true;
+        Experience += exp;  
+    }
+
+    public bool IsAlive()
+    {
+        return Hp > 0;
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpSpeed = 5;
+    public Collider2D bottomCollider;
+    public CompositeCollider2D terrainCollider;
+
+    private Rigidbody2D rb;
+    private Player player;
+
+    private float prevVx = 0;
+    private float vx = 0;
+    private bool isGround;
+    private bool goIdle;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //player 데이터 초기화 (Hp, Damage, Exp)
+        player = new Player(100, 5, 0);
+    }
+
+    void Update()
+    {
+        vx = Input.GetAxisRaw("Horizontal") * speed;
+        float vy = GetComponent<Rigidbody2D>().linearVelocityY;
+
+        if (vx < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (vx > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        //땅에 닿아있는가
+        if (bottomCollider.IsTouching(terrainCollider))
+        {
+            if (!isGround)
+            {
+                if (vx == 0)
+                {
+                    GetComponent<Animator>().SetTrigger("Idle");
+                }
+                else
+                {
+                    GetComponent<Animator>().SetTrigger("Walk");
+                }
+            }
+            else
+            {
+                if (vx != prevVx)
+                {
+                    if (vx == 0)
+                    {
+                        GetComponent<Animator>().SetTrigger("Idle");
+                    }
+                    else
+                    {
+                        GetComponent<Animator>().SetTrigger("Walk");
+                    }
+                }
+                else if(goIdle)
+                {
+                    GetComponent<Animator>().SetTrigger("Idle");
+                    goIdle = false;
+                }
+
+            }
+        }
+        else
+        {
+            if (isGround)
+            {
+                GetComponent<Animator>().SetTrigger("Jump");
+            }
+        }
+
+        //플레이어 발이 바닥 콜라이더와 닿았다면
+        isGround = bottomCollider.IsTouching(terrainCollider);
+
+        if (Input.GetButtonDown("Jump") && isGround == true)
+        {
+            vy = jumpSpeed;
+        }
+        prevVx = vx;
+
+        GetComponent<Rigidbody2D>().linearVelocity = new Vector2(vx, vy);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Animator>().SetTrigger("Attack1");
+            DealDamage(1);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            GetComponent<Animator>().SetTrigger("Attack2");
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GetComponent<Animator>().SetTrigger("BowAttack");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Animator>().SetTrigger("RightAttack");
+        }
+    }
+
+    void DealDamage(int damage) 
+    {
+        Debug.Log($"Dealt {player.Damage * damage} damage!");
+    }
+
+    //애니메이션 스프라이트 문제 해결 함수
+    void UpY()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.4f, transform.position.z);
+        rb.gravityScale = 0;
+        
+        Debug.Log("UPY");
+    }
+    void DownY()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.4f, transform.position.z);
+        rb.gravityScale = 4;
+        isGround = false;
+        Debug.Log("DownY");
+    }
+
+    void SetGoIdle()
+    {
+        goIdle = true;
+    }
+}
